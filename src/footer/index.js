750df70f877421f57097aac5f9ea30786e008a9c
Kauê Trigolo <131711415+kauetrigolo@users.noreply.github.com>
2023-09-14 21:33:06 -0300

    Add files via upload

---
+++ src/footer/index.js
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer(){
+    return(
+        <div className="footer">
+            <footer>Direitos autorais da Stonks</footer>
+        </div>
+    );
+}
+
+export default Footer;
