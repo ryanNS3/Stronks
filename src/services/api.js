03999f473c5e889dd175dd2f792838cb34f6c718
Ryan <131712164+ryanNS3@users.noreply.github.com>
2023-09-26 08:34:37 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
     const api1 = axios.create({
-        baseURL: 'https://brapi.dev/api/quote/MGLU3%2CTSLA34%2CAAPL34%2CD1EL34%2CM1TA34?token=dkXVCDeRrPuBGb4vbicPcD'
+        baseURL: 'https://brapi.dev/api/quote/MGLU3%2CTSLA34%2CAAPL34%2CD1EL34%2CM1TA34?token=gQg7mbczAnCc95BP7eEh1K'
     })
 
     
